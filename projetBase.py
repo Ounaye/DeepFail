15e7f50a5112d0e1c63f12cfcb13e4f58aa7cad1
Bib138 <55283722+Bib138@users.noreply.github.com>
2021-01-25 15:16:10 +0100

    Add files via upload

--- projetBase.py
+++ projetBase.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 18 16:35:44 2021
-
 @author: Ounaye
 """
 
@@ -89,6 +88,10 @@ from skimage import io
 import skimage.transform as sky_trfm
 import numpy as np
 
+from skimage import feature
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 # Si on enlève le filtrage .jpeg 
 # Il a besoin de format de lecture en arg
 
@@ -117,19 +120,81 @@ def rescaleImg(listImg,listNameImg):
 
 
 
-def prepareTabForLearning(tabOfM,tabOfNM):
+def prepareTabForLearning(tabOfM,tabOfNM,threshold):
     tabOfData = zeros((len(tabOfM)+len(tabOfNM),3))
     tabOfResult = np.arange((len(tabOfM)+len(tabOfNM)))
     index = 0
     for i in tabOfM: #1 à 4 secondes d'exécution
-        tabOfData[index] = analyseAllImg(2000,i)# Modifier cette fonction
+        tabOfData[index] = analyseAllImg(threshold,i)# Modifier cette fonction
         tabOfResult[index] = 1
         index +=1
     for i in tabOfNM:
-        tabOfData[index] = analyseAllImg(2000,i)# Modifier cette fonction
+        tabOfData[index] = analyseAllImg(threshold,i)# Modifier cette fonction
         tabOfResult[index] = -1
         index +=1
     return (tabOfData,tabOfResult)
+
+
+def findOutLinesOfImage(pathName):
+#---------- Read Image ----------#
+    img = mpimg.imread(pathName)
+    print (type(img))
+    print (img.shape, img.dtype)
+    print (img[63,63,0],img[63,63,1],img[63,63,2])
+    print (img.max(),img.min())
+
+    M = np.zeros((img.shape[0],img.shape[1]))
+    print (M)
+    M[:,:] = img[:,:,0]
+    print (M.max(),M.min(),M.shape)
+    plt.imshow(M, cmap = plt.get_cmap('gray'))
+    plt.title("Lena Picture")
+    plt.savefig("Lena.png")
+    plt.show()
+#---------- Apply Canny  ----------#
+    edges = feature.canny(M, sigma=2)
+    fig, ax = plt.subplots()
+    ax.imshow(edges, cmap=plt.cm.gray, interpolation='nearest')
+    #ax.axis('off')
+    ax.set_title('Canny Edge Detection')
+    plt.savefig("LenaCanny.png")
+    plt.show()
+    
+
+def findOutLinesOfImage2(pathName):
+
+    im1 = Image.open(pathName)
+    im2 = Image.new("RGB",(64,64))
+    im3 = Image.new("RGB",(64,64))
+    for y in range(64):
+        for x in range(64):
+            p = im1.getpixel((x,y))        
+            r = (p[0]+p[1]+p[2])/3
+            v = r
+            b = r
+            im2.putpixel((x,y),(r,v,b))
+    for y in range(0,63):
+        print (y)
+        for x in range(0,63):
+            pix0 = im2.getpixel((x,y))
+            pix1 = im2.getpixel((x-1,y-1))
+            pix2 = im2.getpixel((x,y-1))
+            pix3 = im2.getpixel((x+1,y-1))
+            pix4 = im2.getpixel((x-1,y))
+            pix5 = im2.getpixel((x+1,y))
+            pix6 = im2.getpixel((x-1,y+1))
+            pix7 = im2.getpixel((x,y+1))
+            pix8 = im2.getpixel((x+1,y+1))
+            r = 8*pix0[0]-pix1[0]-pix2[0]- pix3[0]-pix4[0]-pix5[0]-pix6[0]-pix7[0]-pix8[0]
+        r = r/1
+        r = r+128
+        r = 255-r
+        v = r
+        b = r
+    im3.putpixel((x,y),(r,v,b))
+    im3.save("T:\Seville_contours.png")
+    im3.show()
+
     
 
 
@@ -137,32 +202,8 @@ def prepareTabForLearning(tabOfM,tabOfNM):
 
 
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GroupKFold 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-
-X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-y = np.array([1, 2, 3, 4])
-groups = np.array([0, 0, 2, 2])
-group_kfold = GroupKFold(n_splits=2)
-group_kfold.get_n_splits(X, y, groups)
-
-print(group_kfold)
-GroupKFold(n_splits=2)
-for train_index, test_index in group_kfold.split(X, y, groups):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    print("X_Train ")
-    print(X_train)
-    print("X_test ") 
-    print(X_test)
-    print("y_train ") 
-    print(y_train)
-    print("y_test")
-    print(y_test)
-
-
 
 def makeTraining(tabData,tabResult):
     
@@ -175,11 +216,37 @@ def makeTraining(tabData,tabResult):
     
     #On test
     y_predits = classifieur.predict(X_test)
-    print(accuracy_score(y_test,y_predits))
+    return accuracy_score(y_test,y_predits)
+    
+    
+def findBestThreshold(debut, step):
+    maxTreshold = -800000
+    maxScore = -800000
+    for i in range (debut,4001, step):
+        score = 0
+        for j in range(10):
+            a,b = prepareTabForLearning(image_listM,image_listNM, i)
+            score += makeTraining(a,b)
+        if (score >= maxScore) :
+            maxScore = score
+            maxTreshold = i
+
+    return maxTreshold
+            
 
 
-#a,b = prepareTabForLearning(image_listM,image_listNM)
-#makeTraining(a,b)
+
+findOutLinesOfImage2("Data/Mer/Mer_1/aaaaa.jpeg")
+        
+        
+#v = findBestThreshold(1000, 250)
+#print(v)
+#a,b = prepareTabForLearning(image_listM,image_listNM,1750)
+#print(makeTraining(a, b))
+
+
+
+
 
 
 
